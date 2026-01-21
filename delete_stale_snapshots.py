acdf317c4ba271fb8ad61a7d5e87270454bf2ce4
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-01-21 00:05:31 -0600

    add get all snapshots code

--- delete_stale_snapshots.py
+++ delete_stale_snapshots.py
@@ -1,20 +1,22 @@
+# This script will search for stale snapshots, where volumes are not attached to any running instances and delete them
+
 import boto3
 
 # def lambda_handler(event,context):
 def main():
     ec2=boto3.client('ec2')
-    instances_response=ec2.describe_instances(Filters=[{'Name':"instance-state-name",'Values': ["running"]}])
+    running_instances_response=ec2.describe_instances(Filters=[{'Name':"instance-state-name",'Values': ["running"]}])
     running_instances=set()
 
 
-    for reservation in instances_response["Reservations"]:
+    for reservation in running_instances_response["Reservations"]:
         for instance in reservation["Instances"]:
             running_instances.add(instance["InstanceId"])
 
     print(running_instances)
 
-    volumes_response = ec2.describe_volumes()
-    print(volumes_response)
+    snapshots_response = ec2.describe_snapshots()
+    print(snapshots_response)
 
 if __name__ == "__main__":
     main()
